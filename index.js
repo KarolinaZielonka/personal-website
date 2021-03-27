36f6f8a81497a441fc214b2257210162b885735f
Karolina <k99zielonka@gmail.com>
2021-03-27 21:18:11 +0100

    Change logo animations.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('root', 'assets/particles.json', () =>
-    console.log('everyting is ok!'));
+particlesJS.load("root", "assets/particles.json", () =>
+  console.log("everyting is ok!")
+);
+
+/* logo typing */
+
+let logoSpan = document.getElementById("logo");
+let textForLogo = "kZielonka";
+let i = 0;
+
+function logo() {
+  setTimeout(function () {
+    if (i < textForLogo.length) {
+      logoSpan.innerHTML += textForLogo.charAt(i);
+      i++;
+    }
+    logo();
+  }, 1000);
+}
+
+//logo();
+setInterval(logo, 300);
